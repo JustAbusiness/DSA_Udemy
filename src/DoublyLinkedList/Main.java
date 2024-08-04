3deaca982ed28267861c0c739775e1ce503f2c42
JustABusiness <huysanti123456@gmail.com>
2024-08-04 12:55:34 +0700

    DLL: Constructor

---
+++ src/DoublyLinkedList/Main.java
@@ -0,0 +1,14 @@
+package DoublyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(3);
+
+        myDoublyLinkedList.printList();
+
+
+//        myDoublyLinkedList.printList();
+//        myDoublyLinkedList.reverse();
+//        myDoublyLinkedList.printList();
+    }
+}
